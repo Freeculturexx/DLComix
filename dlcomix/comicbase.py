c598bf4507ab58ed18afe21b3200a650f37cf656
Freeculture <guillaume@lame.homelinux.com>
2010-12-18 11:57:02 +0100

    Improve download last comic

--- dlcomix/comicbase.py
+++ dlcomix/comicbase.py
@@ -7,10 +7,20 @@ import urllib
 import re
 
 
-gocomics_base = {	'calvin_and_hobbes'   : ['http://www.gocomics.com/calvinandhobbes',
-'Calvin and Hobbes', '1984-08-14',],
-		'non_sequitur'     : ['http://www.gocomics.com/nonsequitur','Non Sequitur'],
-        'garfield' : ['http://www.gocomics.com/garfield','Garfield']
+gocomics_base = {'2_cows_and_a_chicken' : ['http://www.gocomics.com/features/290-2cowsandachicken',
+    'http://www.gocomics.com/2cowsandachicken', '2 Cows and a Chicken', '2008-06-30'],
+        'bloom_county' : ['http://www.gocomics.com/features/20-bloomcounty',
+            'http://www.gocomics.com/bloomcounty', 'Bloom County', '1980-12-04'],
+        'calvin_and_hobbes'   : ['http://www.gocomics.com/features/32-calvinandhobbes',
+            'http://www.gocomics.com/calvinandhobbes','Calvin and Hobbes', '1984-08-14',],
+        'for_better_or_for_worse' : ['http://www.gocomics.com/features/64-forbetterorforworse',
+            'http://www.gocomics.com/forbetterorforworse','For Better or For Worse', '1981-11-23'],
+        'foxtrot' : ['http://www.gocomics.com/features/66-foxtrot',
+            'http://www.gocomics.com/foxtrot', 'FoxTrot', '1996-03-11'],
+        'garfield' : ['http://www.gocomics.com/features/72-garfield',
+            'http://www.gocomics.com/garfield','Garfield','1978-06-19'],
+        'non_sequitur'     : ['http://www.gocomics.com/features/112-nonsequitur',
+            'http://www.gocomics.com/nonsequitur','Non Sequitur','1992-02-16']
 	}
 
 
@@ -25,14 +35,15 @@ def gocomics(comic,path=None):
 
     today = today_string()
     today_img = today_string_img()
-    today_url =  "%s/%s" % (gocomics_base[comic][0], today)
-    os.system("wget -O" +comic+" "+today_url)
+    today_url =  "%s/" % gocomics_base[comic][0]
+    os.system("wget -O " +comic+" "+today_url)
 
     file = open(comic,"rb")
     htmlSource = file.read()
-    linksList = re.findall('<link rel="image_src" href="(.*?)" />',htmlSource)
-    for link in linksList:
-        os.system("wget -O " +path+comic+"_"+today_img+".gif "+link)
+    link = re.findall('<link rel="image_src" href="(.*?)" />',htmlSource)
+    file = re.findall('<h1 (.*?)><a href="/(.*?)/">', htmlSource)
+    file = file[0][1].replace('/','_')+".gif"
+    os.system("wget -O " +path+file +" "+link[0])
     os.system("rm " +comic)
 
 
