f57efb4f9126cfe8124ebe701f30d12fabbd81ff
Guillaume LAMÉ <guillaume@lame.homelinux.com>
2011-03-12 15:23:32 +0100

    Fix a bug with encoding sqlite entry

--- dlcomix/dlcomix.py
+++ dlcomix/dlcomix.py
@@ -146,7 +146,7 @@ class DLComix(QMainWindow, Ui_DLComix):
         row = self.sqlite.c.fetchall()
         lenght = len(row)-1
         while i <= lenght:
-            self.comboBox.addItem(row[i][0])
+            self.comboBox.addItem(self.trUtf8(row[i][0]))
             i += 1
         self.sqlite.c.close()
 
@@ -256,11 +256,9 @@ Cela peut prendre un peu de temps"""))
         name = ['']*len(test)
         link = ['']*len(test)
         for i in range (0, len(test)):
-            name[i] = test[i][1].capitalize()
-            name[i] = name[i].replace('é','e')
-            name[i] = name[i].replace('&amp;','&')
+            name[i] = test[i][1].decode('utf-8').capitalize()
             lk = (test[i][0]).replace(' ','')
-            link[i] = ("http://www.gocomics.com/"+lk).decode('utf-8')
+            link[i] = ("http://www.gocomics.com/"+lk)
         os.remove("/tmp/initComic")
         return name, link
 
