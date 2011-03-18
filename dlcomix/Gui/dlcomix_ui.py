88d4abcded21d8aac4fce23d7997fe2dcf44e734
Guillaume LAMÉ <guillaume@lame.homelinux.com>
2011-03-18 16:00:47 +0100

    Fix an install bug

--- dlcomix/Gui/dlcomix_ui.py
+++ dlcomix/Gui/dlcomix_ui.py
@@ -1,43 +1,41 @@
 # -*- coding: utf-8 -*-
 
-"""
-This file is part of DLComix.
-
-    DLComix is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    DLComix is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DLComix.  If not, see <http://www.gnu.org/licenses/>.
-
-"""
+# Form implementation generated from reading ui file 'dlcomix_ui.ui'
+#
+# Created: Thu Mar 17 23:14:46 2011
+#      by: PyQt4 UI code generator 4.8.3
+#
+# WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
 
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
 class Ui_DLComix(object):
     def setupUi(self, DLComix):
-        DLComix.setObjectName("DLComix")
-        DLComix.resize(824, 468)
+        DLComix.setObjectName(_fromUtf8("DLComix"))
+        DLComix.resize(940, 468)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../../Icone/dlcomix.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        DLComix.setWindowIcon(icon)
+        DLComix.setIconSize(QtCore.QSize(48, 48))
         self.centralwidget = QtGui.QWidget(DLComix)
-        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.pushButton = QtGui.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(10, 40, 231, 27))
-        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
         self.pushButton_2.setGeometry(QtCore.QRect(10, 80, 231, 27))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.groupBox = QtGui.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(330, 0, 501, 421))
+        self.groupBox.setGeometry(QtCore.QRect(330, 0, 551, 421))
         self.groupBox.setAcceptDrops(False)
         self.groupBox.setAutoFillBackground(False)
         self.groupBox.setFlat(False)
-        self.groupBox.setObjectName("groupBox")
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
         self.comboBox = QtGui.QComboBox(self.groupBox)
         self.comboBox.setGeometry(QtCore.QRect(60, 110, 301, 25))
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
@@ -50,73 +48,73 @@ class Ui_DLComix(object):
         self.comboBox.setBaseSize(QtCore.QSize(0, 0))
         self.comboBox.setEditable(False)
         self.comboBox.setMaxCount(20000)
-        self.comboBox.setObjectName("comboBox")
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
         self.radioButton = QtGui.QRadioButton(self.groupBox)
         self.radioButton.setGeometry(QtCore.QRect(50, 30, 104, 21))
-        self.radioButton.setObjectName("radioButton")
+        self.radioButton.setObjectName(_fromUtf8("radioButton"))
         self.radioButton_2 = QtGui.QRadioButton(self.groupBox)
         self.radioButton_2.setGeometry(QtCore.QRect(50, 60, 70, 21))
-        self.radioButton_2.setObjectName("radioButton_2")
+        self.radioButton_2.setObjectName(_fromUtf8("radioButton_2"))
         self.checkBox = QtGui.QCheckBox(self.groupBox)
         self.checkBox.setGeometry(QtCore.QRect(50, 170, 271, 21))
-        self.checkBox.setObjectName("checkBox")
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
         self.checkBox_2 = QtGui.QCheckBox(self.groupBox)
         self.checkBox_2.setGeometry(QtCore.QRect(50, 200, 281, 21))
-        self.checkBox_2.setObjectName("checkBox_2")
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
         self.checkBox_3 = QtGui.QCheckBox(self.groupBox)
         self.checkBox_3.setGeometry(QtCore.QRect(50, 230, 251, 21))
-        self.checkBox_3.setObjectName("checkBox_3")
+        self.checkBox_3.setObjectName(_fromUtf8("checkBox_3"))
         self.spinBox = QtGui.QSpinBox(self.groupBox)
         self.spinBox.setEnabled(True)
         self.spinBox.setGeometry(QtCore.QRect(50, 260, 53, 25))
         self.spinBox.setReadOnly(False)
         self.spinBox.setMinimum(1)
         self.spinBox.setMaximum(10)
-        self.spinBox.setProperty("value", 10)
-        self.spinBox.setObjectName("spinBox")
+        self.spinBox.setProperty(_fromUtf8("value"), 10)
+        self.spinBox.setObjectName(_fromUtf8("spinBox"))
         self.label = QtGui.QLabel(self.groupBox)
         self.label.setEnabled(True)
         self.label.setGeometry(QtCore.QRect(150, 265, 131, 16))
         font = QtGui.QFont()
         font.setStrikeOut(False)
         self.label.setFont(font)
-        self.label.setObjectName("label")
+        self.label.setObjectName(_fromUtf8("label"))
         self.pushButton_3 = QtGui.QPushButton(self.groupBox)
         self.pushButton_3.setGeometry(QtCore.QRect(170, 330, 88, 27))
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.line = QtGui.QFrame(self.groupBox)
         self.line.setGeometry(QtCore.QRect(10, 140, 481, 20))
         self.line.setFrameShape(QtGui.QFrame.HLine)
         self.line.setFrameShadow(QtGui.QFrame.Sunken)
-        self.line.setObjectName("line")
+        self.line.setObjectName(_fromUtf8("line"))
         self.line_2 = QtGui.QFrame(self.groupBox)
         self.line_2.setGeometry(QtCore.QRect(10, 300, 481, 20))
         self.line_2.setFrameShape(QtGui.QFrame.HLine)
         self.line_2.setFrameShadow(QtGui.QFrame.Sunken)
-        self.line_2.setObjectName("line_2")
+        self.line_2.setObjectName(_fromUtf8("line_2"))
         self.pushButton_4 = QtGui.QPushButton(self.groupBox)
         self.pushButton_4.setGeometry(QtCore.QRect(370, 210, 101, 31))
-        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
         DLComix.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(DLComix)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 824, 23))
-        self.menubar.setObjectName("menubar")
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 940, 23))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
         self.menuFichier = QtGui.QMenu(self.menubar)
-        self.menuFichier.setObjectName("menuFichier")
+        self.menuFichier.setObjectName(_fromUtf8("menuFichier"))
         self.menuEdition = QtGui.QMenu(self.menubar)
-        self.menuEdition.setObjectName("menuEdition")
+        self.menuEdition.setObjectName(_fromUtf8("menuEdition"))
         self.menuAide = QtGui.QMenu(self.menubar)
-        self.menuAide.setObjectName("menuAide")
+        self.menuAide.setObjectName(_fromUtf8("menuAide"))
         DLComix.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(DLComix)
-        self.statusbar.setObjectName("statusbar")
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
         DLComix.setStatusBar(self.statusbar)
         self.actionQuitter = QtGui.QAction(DLComix)
-        self.actionQuitter.setObjectName("actionQuitter")
+        self.actionQuitter.setObjectName(_fromUtf8("actionQuitter"))
         self.actionPreferences = QtGui.QAction(DLComix)
-        self.actionPreferences.setObjectName("actionPreferences")
+        self.actionPreferences.setObjectName(_fromUtf8("actionPreferences"))
         self.actionA_Propos = QtGui.QAction(DLComix)
-        self.actionA_Propos.setObjectName("actionA_Propos")
+        self.actionA_Propos.setObjectName(_fromUtf8("actionA_Propos"))
         self.menuFichier.addAction(self.actionQuitter)
         self.menuEdition.addAction(self.actionPreferences)
         self.menuAide.addAction(self.actionA_Propos)
